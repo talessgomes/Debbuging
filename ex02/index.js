2c724070e097cb3ff56a6df179f18d9fa35aef1f
Tales Gomes <tales.santos.gomes@outlook.com>
2022-11-17 09:16:03 -0300

    finalizado-exercicio-02

---
+++ ex02/index.js
@@ -0,0 +1,7 @@
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+console.log(output);
+
+console.clear();
+
+// console.clear serve para limpar o console do browser
