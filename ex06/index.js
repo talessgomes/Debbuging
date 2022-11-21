0ab1edb0dc4183e32ad239290328dbd28164ddf5
Tales Gomes <tales.santos.gomes@outlook.com>
2022-11-21 11:24:18 -0300

    finalizado-exercicio-06

---
+++ ex06/index.js
@@ -0,0 +1,12 @@
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml); //<p>Click here to <a href='#Home'>return home</a></p>
+
+/* Exemplos
+
+const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+
+a ideia é usar aspas duplas e simples, no innetHtml, eu vou colocar aspas duplas
+em volta da string, na tag a, eu vou colocar aspas simples no href
+*/
